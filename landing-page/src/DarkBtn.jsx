6eced7758d6ff1b97a843f351369e02b40a0bc51
vzbel <142253332+vzbel@users.noreply.github.com>
2024-11-19 16:49:23 -0700

    Create hero header

---
+++ landing-page/src/DarkBtn.jsx
@@ -0,0 +1,5 @@
+export default function DarkBtn(){
+    return (
+        <button className="bg-black text-white p-3 rounded-lg">Download CV</button>
+    );
+}
---
+++ landing-page/src/LightBtn.jsx
@@ -0,0 +1,5 @@
+export default function LightBtn(){
+    return (
+        <button className="bg-slate-100 p-3 rounded-lg">Contact Me</button>
+    );
+}
